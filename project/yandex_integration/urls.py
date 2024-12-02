ade14112463df4cdc9a119075f236ab6f9a0c760
lasko <vershinineldar@yandex.ru>
2024-12-02 03:11:53 +0300

    feat: public link processing from template

--- project/yandex_integration/urls.py
+++ project/yandex_integration/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
+from .views import URLSubmitView
 
 urlpatterns = [
-    # path(..., ...),
+    path('main-page/', URLSubmitView.as_view(), name='main_page'),
 ]
--- project/yandex_integration/views.py
+++ project/yandex_integration/views.py
@@ -1,3 +1,15 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+class URLSubmitView(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'main_page.html')
+
+    def post(self, request, *args, **kwargs):
+        url = request.POST.get('url')
+        if url:
+            # Логика обработки URL
+            return JsonResponse({'message': 'ok', 'url': url})
+        return JsonResponse({'error': 'not ok'}, status=400)
